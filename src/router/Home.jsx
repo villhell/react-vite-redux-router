b781e7a09afd60ed74f05603389ae8593493bd5a
villhell <lilly.of.the.valley.to@gmail.com>
2022-08-25 22:34:15 +0900

    Added sample to display WebAPI return values

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,26 +1,30 @@
 import { RepositoryFactory } from '../repositories/RepositoryFactory';
-
+import { useEffect, useState } from 'react';
 function Home() {
+  const [x, set] = useState('');
+  const userRepository = RepositoryFactory.get('users');
+
+  useEffect(() => {
+    const userResponse = async () => {
+      const response = await userRepository.show(2);
+      console.log(response.data);
+      //const json = JSON.stringify(response.data);
+      set(response.data);
+    };
+    userResponse();
+  }, []);
+
   return (
     <div>
       <h2>Home</h2>
+      <p>{x.id}</p>
+      <p>{x.name}</p>
+      <p>{x.phone}</p>
+      <p>{x.username}</p>
+      <p>{x.website}</p>
+      <p>{x.email}</p>
     </div>
   );
 }
-
-const userRepository = RepositoryFactory.get('users');
-
-const userResponse = async () => {
-  const showResponse = await userRepository.show(2);
-  console.log('show', showResponse);
-
-  // const indexResponse = await userRepository.index();
-  // console.log('index', indexResponse);
-
-  // const deleteResponse = await userRepository.delete(1);
-  // console.log('delete', deleteResponse);
-};
-
-userResponse();
 
 export default Home;
